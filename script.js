6ee536741d9a5720d49d0d4700f9141a6340c297
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-01-29 15:45:47 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+////////////////* IMPORTANT ALWAYS USE THIS URL: https://countries-api-836d.onrender.com/countries/
