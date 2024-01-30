4bd23fe5813cae21da754e6a0a2db6c6b662752e
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-01-30 10:23:25 +0800

    XMLHttpRequest

--- script.js
+++ script.js
@@ -5,4 +5,40 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const getCountryData = function (country) {
+  // OLD SCHOOL OF REQUESTING
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+            </div>
+            </article>`;
+
+    console.log(html);
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 ////////////////* IMPORTANT ALWAYS USE THIS URL: https://countries-api-836d.onrender.com/countries/
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('philippines');
