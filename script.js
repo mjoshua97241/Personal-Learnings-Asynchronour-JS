2943155701d98ed580f53b7f46f6531111f7ec06
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-01-31 13:22:27 +0800

    Callback Hell

--- script.js
+++ script.js
@@ -5,19 +5,8 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  // OLD SCHOOL OF REQUESTING
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    //   console.log(this.responseText);
-
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `<article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
         <h3 class="country__name">${data.name}</h3>
@@ -32,13 +21,65 @@ const getCountryData = function (country) {
             </div>
             </article>`;
 
-    console.log(html);
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  console.log(html);
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // OLD SCHOOL OF REQUESTING
+
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get Neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 ////////////////* IMPORTANT ALWAYS USE THIS URL: https://countries-api-836d.onrender.com/countries/
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('philippines');
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+// CALLBACK HELL: Messy and hard to understand is a bad code. This will lead to more bugs.
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+        setTimeout(() => {
+          console.log('5 second passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
